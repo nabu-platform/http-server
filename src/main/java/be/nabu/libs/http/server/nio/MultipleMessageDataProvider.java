f1623178f61b1cd296da9b51d6bfa80ceaf26148
alex <alex@nabu.be>
2016-03-19 10:28:30 +0100

    Added a multiple provider

---
+++ src/main/java/be/nabu/libs/http/server/nio/MultipleMessageDataProvider.java
@@ -0,0 +1,36 @@
+package be.nabu.libs.http.server.nio;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import be.nabu.libs.http.api.server.MessageDataProvider;
+import be.nabu.libs.resources.api.ReadableResource;
+import be.nabu.libs.resources.api.WritableResource;
+import be.nabu.libs.resources.memory.MemoryItem;
+import be.nabu.utils.mime.api.Header;
+
+public class MultipleMessageDataProvider implements MessageDataProvider {
+
+	private List<MessageDataProvider> providers = new ArrayList<MessageDataProvider>();
+	
+	public MultipleMessageDataProvider(MessageDataProvider...providers) {
+		if (providers != null && providers.length > 0) {
+			this.providers.addAll(Arrays.asList(providers));
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T extends WritableResource & ReadableResource> T newResource(String method, String target, double version, Header... headers) throws IOException {
+		for (MessageDataProvider provider : providers) {
+			T newResource = provider.newResource(method, target, version, headers);
+			if (newResource != null) {
+				return newResource;
+			}
+		}
+		return (T) new MemoryItem("tmp");
+	}
+
+}
